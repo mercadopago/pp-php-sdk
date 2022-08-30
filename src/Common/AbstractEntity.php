1821c6c2763b8095b81f1aefaefc1f672c7a3bd9
sleaof <stephanie.leao@mercadolivre.com>
2022-08-30 18:08:49 -0300

    feat(PPWP-1283): basis for abstractEntity

--- src/Common/AbstractEntity.php
+++ src/Common/AbstractEntity.php
@@ -2,21 +2,171 @@
 
 namespace MercadoPago\PP\Sdk\Common;
 
+use Exception;
+
 /**
  * Class AbstractEntity
  *
  * @package MercadoPago\PP\Sdk\Common
  */
-class AbstractEntity implements \JsonSerializable
+abstract class AbstractEntity
 {
+    /**
+     * AbstractEntity constructor
+     * 
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+    }
 
     /**
+     * @param $name
+     *
+     * @return mixed
+     */
+    public function get($name)
+    {
+        return $this->{$name};
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     *
+     */
+    public function set($name, $value)
+    {
+        if ($this->propertyExists($name)) {
+            $this->{$name} = $value;
+        }
+    }
+
+    /**
+     * @param $property
+     *
+     * @return bool
+     */
+    protected function propertyExists($property)
+    {
+        return array_key_exists($property, get_object_vars($this));
+    }
+
+    /**
+     * Fill entity from data with nested object creation
+     *
+     * @param $entity
+     * @param $data
+     */
+    protected function fillFromArray($entity, $data)
+    {
+        if ($data) {
+            foreach ($data as $key => $value) {
+                if (!is_null($value)) {
+                    if (is_array($value)) {
+                        $className = 'MercadoPago\\PP\\Sdk\\' . $this->camelize($key);
+                        if (class_exists($className, true)) {
+                            $entity->setValue($key, new $className);
+                            $entity->fillFromArray($this->{$key}, $value);
+                        } else {
+                            $entity->setValue($key, json_decode(json_encode($value)));
+                        }
+                        continue;
+                    }
+                    $entity->setValue($key, $value);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param        $input
+     * @param string $separator
+     *
+     * @return mixed
+     */
+    protected function camelize($input, $separator = '_')
+    {
+        return str_replace($separator, '', ucwords($input, $separator));
+    }
+
+    /**
+     * @param null $attributes
+     *
      * @return array
      */
-    public function jsonSerialize()
+    public function toArray($attributes = null)
     {
-        $properties = get_object_vars($this);
+        $result = null;
 
-        return $properties;
+        if (is_null($attributes)) {
+            $result = get_object_vars($this);
+        } else {
+            $result = array_intersect_key(get_object_vars($this), $attributes);
+        }
+
+        foreach ($result as $key => $value) {
+            if (!is_bool($value) && empty($value)) {
+                unset($result[$key]);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $entity
+     * @param $result
+     * @return mixed
+     */
+    protected function attributesToJson($entity, $result)
+    {
+        if (is_array($entity)) {
+            $attributes = array_filter($entity, function ($entity) {
+                return ($entity !== null && $entity !== false && $entity !== '');
+            });
+        } else {
+            $attributes = $entity->toArray();
+        }
+
+        foreach ($attributes as $key => $value) {
+            if ($value instanceof AbstractEntity || is_array($value)) {
+                $this->attributesToJson($value, $result[$key]);
+            } else {
+                if ($value != null || is_bool($value) || is_numeric($value)) {
+                    $result[$key] = $value;
+                }
+            }
+        }
+
+        return json_encode($result);
+    }
+
+    /**
+     * @param $firstArray
+     * @param $secondArray
+     */
+    protected function arrayDiffRecursive($firstArray, $secondArray)
+    {
+        $difference = [];
+
+        foreach (array_keys($secondArray) as $key) {
+            $secondArray[$key] = $secondArray[$key] instanceof AbstractEntity ? $secondArray[$key]->toArray() : $secondArray[$key];
+            if (array_key_exists($key, $firstArray) && $firstArray[$key] instanceof AbstractEntity) {
+                $firstArray[$key] = $firstArray[$key]->toArray();
+            }
+
+            if (!array_key_exists($key, $firstArray)) {
+                $difference[$key] = $secondArray[$key];
+            } elseif (is_array($firstArray[$key]) && is_array($secondArray[$key])) {
+                $newDiff = $this->arrayDiffRecursive($firstArray[$key], $secondArray[$key]);
+                if (!empty($newDiff)) {
+                    $difference[$key] = $newDiff;
+                }
+            } elseif ($firstArray[$key] !== $secondArray[$key]) {
+                $difference[$key] = $secondArray[$key];
+            }
+        }
+        return $difference;
     }
 }
