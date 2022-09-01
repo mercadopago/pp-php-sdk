a07b22c03b2354b6f9e8224f93d7d503d56d75de
sleaof <stephanie.leao@mercadolivre.com>
2022-09-01 10:59:09 -0300

    feat(PPWP-1283): conversion to object/array

--- src/Common/AbstractEntity.php
+++ src/Common/AbstractEntity.php
@@ -10,93 +10,46 @@ namespace MercadoPago\PP\Sdk\Common;
 abstract class AbstractEntity implements \JsonSerializable
 {
     /**
-     * @param $name
-     *
-     * @return mixed
-     */
-    public function get($name)
-    {
-        return $this->{$name};
-    }
-
-    /**
-     * @param $name
-     * @param $value
-     *
-     */
-    public function set($name, $value)
-    {
-        if ($this->propertyExists($name)) {
-            $this->{$name} = $value;
-        }
-    }
-
-    /**
-     * @param $property
-     *
-     * @return bool
-     */
-    protected function propertyExists($property)
-    {
-        return array_key_exists($property, get_object_vars($this));
-    }
-
-    /**
-     * Fill entity from data with nested object creation
-     *
-     * @param $entity
      * @param $data
      */
-    protected function fillFromArray($entity, $data)
+    public function setData($data)
     {
-        if ($data) {
-            foreach ($data as $key => $value) {
-                if (!is_null($value)) {
-                    if (is_array($value)) {
-                        $className = 'MercadoPago\\PP\\Sdk\\Entity\\' . $this->camelize($key);
-                        if (class_exists($className, true)) {
-                            $entity->set($key, new $className);
-                            $entity->fillFromArray($this->{$key}, $value);
-                        } else {
-                            $entity->set($key, json_decode(json_encode($value)));
-                        }
-                        continue;
-                    }
-                    $entity->set($key, $value);
-                }
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
             }
         }
     }
 
     /**
-     * @param        $input
-     * @param string $separator
-     *
-     * @return mixed
-     */
-    protected function camelize($input, $separator = '_')
-    {
-        return str_replace($separator, '', ucwords($input, $separator));
-    }
-
-    /**
      * Get an array from an object
-     *
-     * @param null $attributes
      *
      * @return array
      */
-    public function toArray($attributes = null)
+    public function toArray()
     {
-        $result = null;
+        $properties = get_object_vars($this);
 
-        if (is_null($attributes)) {
-            $result = get_object_vars($this);
-        } else {
-            $result = array_intersect_key(get_object_vars($this), $attributes);
+        $data = [];
+        foreach ($properties as $property => $value) {
+            if ($value instanceof self) {
+                $data[$property] = $value->toArray();
+                continue;
+            }
+
+            if (($value instanceof \IteratorAggregate) || (is_array($value) && count($value))) {
+                foreach ($value as $index => $item) {
+                    if ($item instanceof self) {
+                        $data[$property][$index] = $item->toArray();
+                    } else {
+                        $data[$property][$index] = $item;
+                    }
+                }
+                continue;
+            }
         }
 
-        return $result;
+        return $data;
     }
 
     /**
