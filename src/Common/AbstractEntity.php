f149aa482150b0db059cef17b2d939b6867a9171
mnader_meli <marcelo.nader@mercadolivre.com>
2023-06-15 16:54:38 -0300

    feat(PPCO-2087): reformat code

--- src/Common/AbstractEntity.php
+++ src/Common/AbstractEntity.php
@@ -52,10 +52,8 @@ abstract class AbstractEntity implements \JsonSerializable, EntityInterface
             return;
         }
 
-        if (
-            is_subclass_of($this->{$name}, AbstractEntity::class) ||
-            is_subclass_of($this->{$name}, AbstractCollection::class)
-        ) {
+        if (is_subclass_of($this->{$name}, AbstractEntity::class) ||
+            is_subclass_of($this->{$name}, AbstractCollection::class)) {
             $this->{$name}->setEntity($value);
         } else {
             $this->{$name} = $value;
