167649fc3a2735e59b694f8474b6cd0fd47afd1f
marcelonader-meli <marcelonader@mercadolivre.com>
2023-06-21 12:02:42 -0300

    feature/PPCO-2089: update publish-tag-release.sh script

--- bin/publish-tag-release.sh
+++ bin/publish-tag-release.sh
@@ -7,31 +7,37 @@ DESTINATION_REPOSITORY_USERNAME=''
 DESTINATION_REPOSITORY_NAME=''
 
 apt-get install jq
-echo $(jq --version)
 
 # Verify that there (potentially) some access to the destination repository
 # and set up git (with GIT_CMD variable) and GIT_CMD_REPOSITORY
 if [ -n "${SSH_SDK_DEPLOY_KEY:=}" ]; then
-	echo "[+] Using SSH_SDK_DEPLOY_KEY"
+    echo "[+] Using SSH_SDK_DEPLOY_KEY"
 
-	mkdir -p "$HOME/.ssh"
-	DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
-	echo "${SSH_SDK_DEPLOY_KEY}" >"$DEPLOY_KEY_FILE"
-	chmod 600 "$DEPLOY_KEY_FILE"
+    mkdir -p "$HOME/.ssh"
+    DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
+    echo "${SSH_SDK_DEPLOY_KEY}" >"$DEPLOY_KEY_FILE"
+    chmod 600 "$DEPLOY_KEY_FILE"
 
-	SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
-	ssh-keyscan -H "github.com" >"$SSH_KNOWN_HOSTS_FILE"
+    SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+    ssh-keyscan -H "github.com" >"$SSH_KNOWN_HOSTS_FILE"
 
-	git config --global user.name "$GIT_USER_NAME"
-	git config --global user.email "$GIT_USER_EMAIL"
+    git config --global user.name "$GIT_USER_NAME"
+    git config --global user.email "$GIT_USER_EMAIL"
 
-	export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+    export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
 
-	GIT_CMD_REPOSITORY="git@github.com:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+    GIT_CMD_REPOSITORY="git@github.com:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
 else
-	echo "::error::SSH_SDK_DEPLOY_KEY is empty."
-	exit 1
+    echo "::error::SSH_SDK_DEPLOY_KEY is empty."
+    exit 1
 fi
+
+cd "$SOURCE_DIRECTORY"
+### Preparing release folder
+# rm -rf .git .github .gitmodules .fury .pre-commit-config.yaml bin/publish-tag-release.sh
+# rm -rf .github .gitmodules .fury .pre-commit-config.yaml bin/publish-tag-release.sh
+rm -rf .git bin/publish-tag-release.sh
+
 
 CLONE_DIR=$(mktemp -d)
 
@@ -40,18 +46,24 @@ git --version
 
 echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 {
-	git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
+    git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
 } || {
-	echo "::error::Could not clone the destination repository. Command:"
-	echo "::error::git clone --single-branch $GIT_CMD_REPOSITORY $CLONE_DIR"
-	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
-	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the SSH_DEPLOY_KEY"
-	exit 1
+    echo "::error::Could not clone the destination repository. Command:"
+    echo "::error::git clone --single-branch $GIT_CMD_REPOSITORY $CLONE_DIR"
+    echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
+    echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the SSH_DEPLOY_KEY"
+    exit 1
 }
 ls -la "$CLONE_DIR"
 
 echo "[+] List contents of $SOURCE_DIRECTORY"
 ls "$SOURCE_DIRECTORY"
+
+cd "$CLONE_DIR"
+echo "[+] Removing contents of public repository folder"
+find . -maxdepth 1 -not -name ".git" -exec rm -r {} +
+
+cd "$SOURCE_DIRECTORY"
 
 echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $CLONE_DIR in git repo $DESTINATION_REPOSITORY_NAME"
 cp -Ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/"
@@ -84,3 +96,8 @@ git checkout master
 git pull origin master
 git merge "$TARGET_BRANCH"
 git push origin master
+
+echo "[+] git tag:"
+git tag "v$TAG_VERSION" master
+
+git push origin "v$TAG_VERSION"
